caf4a5b23dc01d18c84b1a85e3bfdab94d7077bf
Carlos Morgado <carlos@paulomorgado.net>
2021-03-29 20:04:16 +0100

    feat(search): new search method

--- src/EntitityFrameworkCore.SearchExtensions/SearchExtensions.cs
+++ src/EntitityFrameworkCore.SearchExtensions/SearchExtensions.cs
@@ -8,8 +8,54 @@ namespace EntitityFrameworkCore.SearchExtensions
 {
     public static class SearchExtensions
     {
+        public static IQueryable<T> Search<T, TProperty>(
+            this IQueryable<T> source,
+            Expression<Func<T, TProperty>> properptySelector,
+            string searchTerm)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (properptySelector is null || string.IsNullOrEmpty(searchTerm))
+            {
+                return source;
+            }
+
+            searchTerm = $"%{searchTerm}%";
+            var itemParameter = Parameter(typeof(T), "item");
+
+            var functions = Property(null, typeof(EF).GetProperty(nameof(EF.Functions)));
+            var like = typeof(DbFunctionsExtensions)
+                .GetMethod(nameof(DbFunctionsExtensions.Like), new Type[] { functions.Type, typeof(string), typeof(string) });
+
+            Expression expressionProperty = properptySelector;
+
+            if (typeof(TProperty) != typeof(string))
+            {
+                expressionProperty = Call(
+                    expressionProperty,
+                    typeof(object).GetMethod(nameof(object.ToString), Array.Empty<Type>()));
+            }
+
+            var selector = Call(
+                       null,
+                       like,
+                       functions,
+                       expressionProperty,
+                       Constant(searchTerm));
+
+            return source.Where(Lambda<Func<T, bool>>(selector, itemParameter));
+        }
+
         public static IQueryable<T> Search<T>(this IQueryable<T> source, string propertyName, string searchTerm)
         {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
             if (string.IsNullOrEmpty(propertyName) || string.IsNullOrEmpty(searchTerm))
             {
                 return source;
@@ -22,17 +68,20 @@ namespace EntitityFrameworkCore.SearchExtensions
                 return source;
             }
 
-            searchTerm = "%" + searchTerm + "%";
+            searchTerm = $"%{searchTerm}%";
             var itemParameter = Parameter(typeof(T), "item");
 
             var functions = Property(null, typeof(EF).GetProperty(nameof(EF.Functions)));
-            var like = typeof(DbFunctionsExtensions).GetMethod(nameof(DbFunctionsExtensions.Like), new Type[] { functions.Type, typeof(string), typeof(string) });
+            var like = typeof(DbFunctionsExtensions)
+                .GetMethod(nameof(DbFunctionsExtensions.Like), new Type[] { functions.Type, typeof(string), typeof(string) });
 
             Expression expressionProperty = Property(itemParameter, property.Name);
 
             if (property.PropertyType != typeof(string))
             {
-                expressionProperty = Call(expressionProperty, typeof(object).GetMethod(nameof(object.ToString), new Type[0]));
+                expressionProperty = Call(
+                    expressionProperty,
+                    typeof(object).GetMethod(nameof(object.ToString), Array.Empty<Type>()));
             }
 
             var selector = Call(
@@ -57,14 +106,14 @@ namespace EntitityFrameworkCore.SearchExtensions
                 return source;
             }
 
-            searchTerm = $"%{searchTerm}%";
-
             var properties = typeof(T).GetProperties();
 
-            if (!properties.Any())
+            if (properties.Length == 0)
             {
                 return source;
             }
+
+            searchTerm = $"%{searchTerm}%";
 
             Expression selector = null;
 
@@ -89,7 +138,7 @@ namespace EntitityFrameworkCore.SearchExtensions
                 {
                     expressionProperty = Call(
                         expressionProperty,
-                        typeof(object).GetMethod(nameof(object.ToString), new Type[0]));
+                        typeof(object).GetMethod(nameof(object.ToString), Array.Empty<Type>()));
                 }
 
                 var newSelector = Call(
